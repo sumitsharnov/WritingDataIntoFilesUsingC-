52095d0e12c19430eba5a0cf627197f92e26d311
sumitsharnov <41141893+sumitsharnov@users.noreply.github.com>
2019-11-25 17:11:46 -0500

    Code to Write data into files using C#

---
+++ MainClass.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_Information_Encoding_4
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+            //-------------------------------------------Making objecct of WordDocument Class file--------------------------------------------
+            WordDocument wordDocument = new WordDocument();
+            wordDocument.WritingInWordDocument();
+
+
+            //-------------------------------------------Making objecct of SpreadSheet Class file--------------------------------------------
+            SpreadSheet ss = new SpreadSheet();
+                ss.creatingSpreadSheet();
+
+            //-------------------------------------------Making objecct of PPTFile Class file---------------------------------------------------------
+            PPTFile pptFile = new PPTFile();
+            string filepath = @"C:\\Users\\Public\\Music\\info.pptx";
+            pptFile.CreatePresentation(filepath);
+        }      
+        
+    }
+} 
+    
---
+++ WordDocument.cs
@@ -0,0 +1,28 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_Information_Encoding_4
+{
+    class WordDocument
+    {
+        public void  WritingInWordDocument()
+        {
+            using (DocumentFormat.OpenXml.Packaging.WordprocessingDocument wordDocument =
+            WordprocessingDocument.Create(@"C:\\Users\\Public\\Music\\info.doc", WordprocessingDocumentType.Document))
+            {
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                // Create the document structure and add some text.
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                Paragraph para = body.AppendChild(new Paragraph());
+                Run run = para.AppendChild(new Run());
+                run.AppendChild(new Text("Hello, my name is Sumit."));
+            }
+        }
+    }
+}
